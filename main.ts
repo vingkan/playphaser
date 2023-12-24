b7cdac270038b38533417257404cefe20adc6c26
Vinesh Kannan <vingkan@gmail.com>
2023-12-23 22:51:14 -0600

    Refactor configurable GameScene class.

--- main.ts
+++ main.ts
@@ -1,7 +1,30 @@
-import { Direction, GridEngine } from "grid-engine";
-import * as Phaser from "phaser";
+import { Direction, GridEngine } from "grid-engine"
+import * as Phaser from "phaser"
 
+type PositionDict = { x: number, y: number }
+type FacingDirection = "up" | "down" | "left" | "right"
+
+type Interaction = (scene: Phaser.Scene) => Promise<void>
+type SceneInteractionMap = { [id: string]: Interaction }
+
+type TileSet = {
+  name: string,
+  path: string
+}
+type TileMap = {
+  path: string,
+  tileSets: TileSet[]
+}
+
+const PLAYER_ID = "player"
+const PLAYER_SCALE = 1.5
+const PLAYER_SPRITE_SHEET = "assets/characters.png"
+// Index in the sprite sheet file, left to right, top to bottom, zero-indexed.
 const PLAYER_SPRITE_INDEX = 7
+const PLAYER_SPRITE_SIZE = {
+  frameWidth: 52,
+  frameHeight: 72,
+}
 
 const TILE_SIZE_PX = 16
 const SCALE = 3
@@ -12,17 +35,99 @@ const TEXT_COLOR = "#625E69"
 const TEXT_SIZE = 24
 const TEXT_PAD = 8
 
-type PositionDict = {x: number, y: number}
-type FacingDirection = "up" | "down" | "left" | "right"
-
 function Facing(x: number, y: number, direction: FacingDirection): string {
   return `${x}_${y}_${direction}`
 }
 
-type Interaction = (scene: Phaser.Scene) => Promise<void>
-type SceneInteractionMap = { [id: string]: Interaction }
+export class GameScene extends Phaser.Scene {
+  private gridEngine!: GridEngine
+  private tileMap!: TileMap
+  private startPosition: PositionDict = { x: 0, y: 0 }
+  private sceneInteractionMap: SceneInteractionMap = {}
 
-const sleep = (ms: number) => new Promise((r) => setTimeout(r, ms));
+  constructor(config: {
+    key: string,
+    tileMap: TileMap,
+    startPosition: PositionDict,
+    sceneInteractionMap: SceneInteractionMap
+  }) {
+    super({
+      key: config.key,
+      active: false,
+      visible: false,
+    })
+    this.tileMap = config.tileMap
+    this.startPosition = config.startPosition
+    this.sceneInteractionMap = config.sceneInteractionMap
+  }
+
+  create() {
+    const tileData = this.tileMap
+    const tileMap = this.make.tilemap({ key: tileData.path })
+    tileData.tileSets.forEach((ts) => tileMap.addTilesetImage(ts.name, ts.path))
+    const tileSetNames = tileData.tileSets.map((ts) => ts.name)
+
+    tileMap.layers.forEach((layerData, i) => {
+      const layer = tileMap.createLayer(i, tileSetNames, 0, 0)
+      layer.scale = SCALE
+    })
+
+    const playerSprite = this.add.sprite(0, 0, PLAYER_ID)
+    playerSprite.scale = PLAYER_SCALE
+    this.cameras.main.startFollow(playerSprite, true)
+    this.cameras.main.setFollowOffset(
+      -playerSprite.width,
+      -playerSprite.height
+    )
+
+    const gridEngineConfig = {
+      characters: [
+        {
+          id: PLAYER_ID,
+          sprite: playerSprite,
+          walkingAnimationMapping: PLAYER_SPRITE_INDEX,
+          startPosition: this.startPosition,
+        },
+      ],
+    }
+    this.gridEngine.create(tileMap, gridEngineConfig)
+  }
+
+  public update() {
+    const cursors = this.input.keyboard.createCursorKeys()
+    if (cursors.left.isDown) {
+      this.gridEngine.move(PLAYER_ID, Direction.LEFT)
+    } else if (cursors.right.isDown) {
+      this.gridEngine.move(PLAYER_ID, Direction.RIGHT)
+    } else if (cursors.up.isDown) {
+      this.gridEngine.move(PLAYER_ID, Direction.UP)
+    } else if (cursors.down.isDown) {
+      this.gridEngine.move(PLAYER_ID, Direction.DOWN)
+    }
+
+    if (cursors.space.isDown) {
+      const dir = this.gridEngine.getFacingDirection(PLAYER_ID)
+      const pos = this.gridEngine.getFacingPosition(PLAYER_ID)
+      const facing = Facing(pos.x, pos.y, dir as FacingDirection)
+      const doInteract = this.sceneInteractionMap?.[facing]
+      if (doInteract) {
+        doInteract(this)
+      }
+    }
+
+  }
+
+  preload() {
+    const scene = this
+    const tileData = scene.tileMap
+    tileData.tileSets.forEach((ts) => scene.load.image(ts.path, ts.path))
+    scene.load.tilemapTiledJSON(tileData.path, tileData.path)
+
+    scene.load.spritesheet(PLAYER_ID, PLAYER_SPRITE_SHEET, PLAYER_SPRITE_SIZE)
+  }
+}
+
+const sleep = (ms: number) => new Promise((r) => setTimeout(r, ms))
 
 function showText(scene: Phaser.Scene, x: number, y: number, text: string): Phaser.GameObjects.Text {
   const tX = TEXT_PAD + (x * TILE_SIZE_SCALED)
@@ -66,189 +171,53 @@ const doReadTablet = interactIfNotStarted(readTablet)
 const doSpeakStatue = interactIfNotStarted(speakStatue)
 const doGoToForestTemple = interactIfNotStarted(goToForestTemple)
 
-export class SkyCityScene extends Phaser.Scene {
-  private gridEngine!: GridEngine
-  private startPosition: PositionDict = { x: 10, y: 14 }
-  private sceneInteractionMap: SceneInteractionMap = {
-    [Facing(9, 13, 'up')]: doReadTablet,
-    [Facing(10, 13, 'up')]: doReadTablet,
-    [Facing(9, 6, 'down')]: doSpeakStatue,
-    [Facing(10, 6, 'down')]: doSpeakStatue,
-    [Facing(8, 10, 'up')]: doGoToForestTemple,
-    [Facing(9, 10, 'up')]: doGoToForestTemple,
-    [Facing(10, 10, 'up')]: doGoToForestTemple,
-    [Facing(10, 10, 'up')]: doGoToForestTemple,
-  }
-
+export class SkyCityScene extends GameScene {
   constructor() {
     super({
       key: "SkyCityScene",
-      active: false,
-      visible: false,
-    });
-  }
-
-  create() {
-    const cloudCityTilemap = this.make.tilemap({ key: "sky-city-map" });
-    cloudCityTilemap.addTilesetImage("Cloud City", "tiles");
-    for (let i = 0; i < cloudCityTilemap.layers.length; i++) {
-      const layer = cloudCityTilemap.createLayer(i, "Cloud City", 0, 0);
-      layer.scale = 3;
-    }
-    const playerSprite = this.add.sprite(0, 0, "player");
-    playerSprite.scale = 1.5;
-    this.cameras.main.startFollow(playerSprite, true);
-    this.cameras.main.setFollowOffset(
-      -playerSprite.width,
-      -playerSprite.height
-    );
-
-    const gridEngineConfig = {
-      characters: [
-        {
-          id: "player",
-          sprite: playerSprite,
-          walkingAnimationMapping: PLAYER_SPRITE_INDEX,
-          startPosition: this.startPosition,
-        },
-      ],
-    };
-
-    this.gridEngine.create(cloudCityTilemap, gridEngineConfig);
-  }
-
-  public update() {
-    const cursors = this.input.keyboard.createCursorKeys();
-    if (cursors.left.isDown) {
-      this.gridEngine.move("player", Direction.LEFT);
-    } else if (cursors.right.isDown) {
-      this.gridEngine.move("player", Direction.RIGHT);
-    } else if (cursors.up.isDown) {
-      this.gridEngine.move("player", Direction.UP);
-    } else if (cursors.down.isDown) {
-      this.gridEngine.move("player", Direction.DOWN);
-    }
-
-    if (cursors.space.isDown) {
-      const dir = this.gridEngine.getFacingDirection("player")
-      const pos = this.gridEngine.getFacingPosition("player")
-      const facing = Facing(pos.x, pos.y, dir as FacingDirection)
-      const doInteract = this.sceneInteractionMap?.[facing]
-      if (doInteract) {
-        doInteract(this)
-      }
-    }
-
-  }
-
-  preload() {
-    this.load.image("tiles", "assets/cloud_tileset.png");
-    this.load.tilemapTiledJSON("sky-city-map", "assets/sky-city.json");
-
-    this.load.spritesheet("player", "assets/characters.png", {
-      frameWidth: 52,
-      frameHeight: 72,
-    });
+      tileMap: {
+        path: "assets/sky-city.json",
+        tileSets: [
+          { name: "Cloud City", path: "assets/cloud_tileset.png" },
+        ]
+      },
+      startPosition: { x: 10, y: 14 },
+      sceneInteractionMap: {
+        [Facing(9, 13, 'up')]: doReadTablet,
+        [Facing(10, 13, 'up')]: doReadTablet,
+        [Facing(9, 6, 'down')]: doSpeakStatue,
+        [Facing(10, 6, 'down')]: doSpeakStatue,
+        [Facing(8, 10, 'up')]: doGoToForestTemple,
+        [Facing(9, 10, 'up')]: doGoToForestTemple,
+        [Facing(10, 10, 'up')]: doGoToForestTemple,
+        [Facing(10, 10, 'up')]: doGoToForestTemple,
+      },
+    })
   }
 }
 
-export class ForestTempleScene extends Phaser.Scene {
-  private gridEngine!: GridEngine
-  private startPosition: PositionDict = { x: 9, y: 17 }
-  private sceneInteractionMap: SceneInteractionMap = {
-    // [Facing(9, 13, 'up')]: doReadTablet,
-  }
-
+export class ForestTempleScene extends GameScene {
   constructor() {
     super({
       key: "ForestTempleScene",
-      active: false,
-      visible: false,
-    });
-  }
-
-  create() {
-    const forestTempleTilemap = this.make.tilemap({ key: "forest-temple-map" });
-    forestTempleTilemap.addTilesetImage("Grass", "grass");
-    forestTempleTilemap.addTilesetImage("Wall", "wall")
-    forestTempleTilemap.addTilesetImage("Structures", "structures")
-    forestTempleTilemap.addTilesetImage("Props", "props")
-    forestTempleTilemap.addTilesetImage("Collisions", "collisions")
-    const tilesetNames = [
-      "Grass",
-      "Wall",
-      "Structures",
-      "Props",
-      "Collisions",
-    ]
-    for (let i = 0; i < forestTempleTilemap.layers.length; i++) {
-      const layer = forestTempleTilemap.createLayer(i, tilesetNames, 0, 0);
-      layer.scale = 3;
-    }
-    const playerSprite = this.add.sprite(0, 0, "player");
-    playerSprite.scale = 1.5;
-    this.cameras.main.startFollow(playerSprite, true);
-    this.cameras.main.setFollowOffset(
-      -playerSprite.width,
-      -playerSprite.height
-    );
-
-    const gridEngineConfig = {
-      characters: [
-        {
-          id: "player",
-          sprite: playerSprite,
-          walkingAnimationMapping: PLAYER_SPRITE_INDEX,
-          startPosition: this.startPosition,
-        },
-      ],
-    };
-
-    this.gridEngine.create(forestTempleTilemap, gridEngineConfig);
-  }
-
-  public update() {
-    const cursors = this.input.keyboard.createCursorKeys();
-    if (cursors.left.isDown) {
-      this.gridEngine.move("player", Direction.LEFT);
-    } else if (cursors.right.isDown) {
-      this.gridEngine.move("player", Direction.RIGHT);
-    } else if (cursors.up.isDown) {
-      this.gridEngine.move("player", Direction.UP);
-    } else if (cursors.down.isDown) {
-      this.gridEngine.move("player", Direction.DOWN);
-    }
-
-    if (cursors.space.isDown) {
-      const dir = this.gridEngine.getFacingDirection("player")
-      const pos = this.gridEngine.getFacingPosition("player")
-      const facing = Facing(pos.x, pos.y, dir as FacingDirection)
-      const doInteract = this.sceneInteractionMap?.[facing]
-      if (doInteract) {
-        doInteract(this)
-      }
-    }
-
-  }
-
-  preload() {
-    this.load.image("grass", "assets/forest/tileset_grass.png");
-    this.load.image("wall", "assets/forest/tileset_wall.png");
-    this.load.image("stone", "assets/forest/tileset_stone.png");
-    this.load.image("structures", "assets/forest/structures.png");
-    this.load.image("props", "assets/forest/props.png");
-    this.load.image("collisions", "assets/collisions.png");
-    this.load.tilemapTiledJSON("forest-temple-map", "assets/forest-temple.json");
-
-    this.load.spritesheet("player", "assets/characters.png", {
-      frameWidth: 52,
-      frameHeight: 72,
-    });
+      tileMap: {
+        path: "assets/forest-temple.json",
+        tileSets: [
+          { name: "Grass", path: "assets/forest/tileset_grass.png" },
+          { name: "Wall", path: "assets/forest/tileset_wall.png" },
+          { name: "Structures", path: "assets/forest/structures.png" },
+          { name: "Props", path: "assets/forest/props.png" },
+          { name: "Collisions", path: "assets/collisions.png" },
+        ]
+      },
+      startPosition: { x: 9, y: 17 },
+      sceneInteractionMap: {},
+    })
   }
 }
 
 const gameConfig: Phaser.Types.Core.GameConfig = {
-  title: "Sample",
+  title: "Curse of the Aztecs",
   render: {
     antialias: false,
   },
@@ -269,6 +238,6 @@ const gameConfig: Phaser.Types.Core.GameConfig = {
     ],
   },
   parent: "game",
-};
+}
 
-export const game = new Phaser.Game(gameConfig);
+export const game = new Phaser.Game(gameConfig)
