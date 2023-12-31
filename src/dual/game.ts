163572f6b73cf8a0b28ff812e5f9aed68d0a84de
Vinesh Kannan <vingkan@gmail.com>
2023-12-30 20:12:44 -0600

    Run both games in same frame.

--- src/dual/game.ts
+++ src/dual/game.ts
@@ -2,80 +2,106 @@ import { GridEngine } from "grid-engine"
 import * as Phaser from "phaser"
 import { GameScene } from "./scene"
 
-declare global {
-    interface Window {
-        VARIANT: string
-    }
+type GameVariant = {
+    game: Phaser.Game,
+    setPressedKey: (key: string | null) => void,
 }
 
-const variant = window.VARIANT
+function GameFactory(variant: string): GameVariant {
+    let setPressedKey: (key: string | null) => void = () => {}
 
-let setPressedKey: (key: string | null) => void = () => {}
+    class SeaScene extends GameScene {
+        constructor() {
+            super({
+                key: `${variant}Scene`,
+                variant,
+                tileMap: {
+                    path: "../assets/dual.json",
+                    tileSets: [
+                        { name: "Collisions", path: "../assets/common/collisions.png" },
+                        { name: "Ground", path: `../assets/sea/ground-${variant}.png` },
+                        { name: "Props", path: `../assets/sea/props-${variant}.png` },
+                    ]
+                },
+                startCharLayer: "Collisions",
+                startPosition: { x: 3, y: 18 },
+                sceneInteractionMap: {},
+            })
+        }
 
-class SeaScene extends GameScene {
-    constructor() {
-        super({
-            key: `${variant}Scene`,
-            variant,
-            tileMap: {
-                path: "../assets/dual.json",
-                tileSets: [
-                    { name: "Collisions", path: "../assets/common/collisions.png" },
-                    { name: "Ground", path: `../assets/sea/ground-${variant}.png` },
-                    { name: "Props", path: `../assets/sea/props-${variant}.png` },
-                ]
-            },
-            startCharLayer: "Collisions",
-            startPosition: { x: 3, y: 18 },
-            sceneInteractionMap: {},
-        })
+        preloadThen() {
+            const scene = this
+        }
+
+        createThen() {
+            const scene = this
+            setPressedKey = (key) => scene.setPressedKey(key)
+        }
     }
 
-    preloadThen() {
-        const scene = this
-    }
-
-    createThen() {
-        const scene = this
-        setPressedKey = (key) => scene.setPressedKey(key)
-    }
-}
-
-const title = `${variant[0].toUpperCase()}${variant.substring(1)}`
-const parent = "game"
-const gameConfig: Phaser.Types.Core.GameConfig = {
-    title,
-    parent,
-    type: Phaser.AUTO,
-    scene: [SeaScene],
-    scale: {
-        mode: Phaser.Scale.FIT,
-        autoCenter: Phaser.Scale.CENTER_BOTH,
+    const title = `${variant[0].toUpperCase()}${variant.substring(1)}`
+    const parent = variant
+    const gameConfig: Phaser.Types.Core.GameConfig = {
+        title,
         parent,
-    },
-    render: {
-        antialias: false,
-    },
-    fps: {
-        smoothStep: false,
-    },
-    plugins: {
-        scene: [
-            {
-                key: "gridEngine",
-                plugin: GridEngine,
-                mapping: "gridEngine",
-            },
-        ],
-    },
+        type: Phaser.AUTO,
+        scene: [SeaScene],
+        scale: {
+            mode: Phaser.Scale.FIT,
+            autoCenter: Phaser.Scale.CENTER_BOTH,
+            parent,
+        },
+        render: {
+            antialias: false,
+        },
+        fps: {
+            smoothStep: false,
+        },
+        plugins: {
+            scene: [
+                {
+                    key: "gridEngine",
+                    plugin: GridEngine,
+                    mapping: "gridEngine",
+                },
+            ],
+        },
+    }
+
+    const game = new Phaser.Game(gameConfig)
+    return {
+        game,
+        setPressedKey: (key) => setPressedKey(key),
+    }
 }
 
-const game = new Phaser.Game(gameConfig)
+const seaGame = GameFactory("sea")
+const sandGame = GameFactory("sand")
 
-window.addEventListener("message", (e) => {
-    const isSameOrigin = window.origin === e.origin && e.isTrusted
-    if (!isSameOrigin) return
+const ALLOWED_KEYS = {
+    "ArrowUp": "ArrowUp",
+    "ArrowDown": "ArrowDown",
+    "ArrowLeft": "ArrowLeft",
+    "ArrowRight": "ArrowRight",
+    "Space": "Space",
+}
 
-    const key = e.data.key
-    setPressedKey(key)
+let pressedKey = null
+
+function maybeSendNewKey(newKey) {
+    if (newKey === pressedKey) return
+    pressedKey = newKey
+    seaGame.setPressedKey(pressedKey)
+    sandGame.setPressedKey(pressedKey)
+}
+
+document.addEventListener("keydown", (e) => {
+    const key = e.key
+    const newKey = ALLOWED_KEYS?.[key] || null
+    maybeSendNewKey(newKey)
+})
+
+document.addEventListener("keyup", (e) => {
+    const newKey = null
+    maybeSendNewKey(newKey)
 })
